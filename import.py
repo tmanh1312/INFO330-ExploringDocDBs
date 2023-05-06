d4dc90956c57a5a46f56e928b3e60a7c675ebf8b
Mia Truong <tmanh1312@gmail.com>
2023-05-06 13:39:11 -0700

    import and 3 queries

---
+++ import.py
@@ -0,0 +1,40 @@
+# mongoimport --db=pokemondb --collection=pokemon_data --type=csv --fields name,pokedex_number,type1,type2,hp,attack,defense,speed,sp_attack,sp_defense,abilities --file pokemon.csv
+
+import sqlite3
+from pymongo import MongoClient
+
+# Connect to SQLite database
+sqlite_conn = sqlite3.connect('pokemon.sqlite')
+sqlite_cur = sqlite_conn.cursor()
+
+# Connect to MongoDB
+mongoClient = MongoClient("mongodb://localhost/pokemon")
+pokemonDB = mongoClient['pokemondb']
+pokemonColl = pokemonDB['pokemon_data']
+
+# Execute a SELECT query on SQLite
+sqlite_query = '''SELECT p.name, p.pokedex_number, p.type1, p.type2, p.hp, p.attack, p.defense, p.speed, p.sp_attack, p.sp_defense, p.abilities
+                  FROM imported_pokemon_data AS p
+                  '''
+sqlite_cur.execute(sqlite_query)
+
+# Iterate through each row and add to MongoDB
+for row in sqlite_cur.fetchall():
+    pokemon = {
+        "name": row[0],
+        "pokedex_number": row[1],
+        "types": [row[2], row[3]], # type1 and type2
+        "hp": row[4],
+        "attack": row[5],
+        "defense": row[6],
+        "speed": row[7],
+        "sp_attack": row[8],
+        "sp_defense": row[9],
+        "abilities": [row[10]] 
+    }
+    pokemonColl.insert_one(pokemon)
+
+# Close the connections
+sqlite_cur.close()
+sqlite_conn.close()
+mongoClient.close()
---
+++ queries.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pymongo import MongoClient
+
+# Connect to MongoDB
+mongoClient = MongoClient("mongodb://localhost/pokemon")
+pokemonDB = mongoClient['pokemondb']
+pokemonColl = pokemonDB['pokemon_data']
+
+# Return all the Pokemon named "Pikachu"
+pikachus = pokemonColl.find({"name": "Pikachu"})
+for p in pikachus:
+    print(p)
+
+# Return all the Pokemon with an attack greater than 150
+powerful_pokemons = pokemonColl.find({"attack": {"$gt": 150}})
+for pokemon in powerful_pokemons:
+    print(pokemon)
+
+# Return all the Pokemon with an ability of "Overgrow"
+overgrow_pokemons = pokemonColl.find({"abilities": {"$in": ["Overgrow"]}})
+for o in overgrow_pokemons:
+    print(o)
+
+
